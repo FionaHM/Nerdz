b15406bad04968f7a71bc461b4ad215ebcbbaa88
charlieEWest <charleswest33@gmail.com>
2017-02-08 15:09:15 -0600

    update seed data with real questions

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+CREATE database nerdz_db;
 drop database nerdz_db;
 CREATE database nerdz_db;
 
@@ -9,50 +10,50 @@ insert into Users (username, email, password, location, overall_category, nerd_l
 ("user2" , "user2@test.com", "test123", "IL", "CAT B", "Nerd",now() , now());
 
 insert into Categories (category_name) values 
-("CAT A"),
-("CAT B"),
-("CAT C" ),
-("CAT D" ),
-("CAT E" );
+("SCIENCE"),
+("MUSIC"),
+("TECHNOLOGY" ),
+("CULTURE" ),
+("ENGINEERING" );
 
 insert into Questions (question, category, created_at, updated_at) values 
-("What day is today", "CAT A",now() , now() ),
-("How old are you?", "CAT A", now(),now()),
-("When is Monday?", "CAT C", now(), now()),
-("When is Tuesday?", "CAT C", now(), now()),
-("When is Wednesday?", "CAT B", now(), now()),
-("When is Thursday?", "CAT B", now(), now()),
-("When is Friday?", "CAT D", now(), now()),
-("When is Saturday?", "CAT D", now(), now()),
-("When is Sunday?", "CAT E", now(), now()),
-("When is May?", "CAT E", now(), now());
+("I like science fiction.", "science, culture, engineering",now() , now() ),
+("I sometimes prefer fictional people to real ones.", "science, technology", now(),now()),
+("I have started writing a novel.", "culture, music", now(), now()),
+("I come up with something new ", "science, engineering", now(), now()),
+("I like to read Biographys.", "culture", now(), now()),
+("Enjoy going to fiction conventions.", "scinence, engineering, technology", now(), now()),
+("I have a strong need to correct inaccurate information.", "engineering", now(), now()),
+("I like staying indoors.", "science, technology, engineering", now(), now()),
+("I prefer to spend time with machines than with people.", "technology, engineering", now(), now()),
+("I love to read science fiction novels.", "science, technology, engineering", now(), now()),
+("I have a collection of figurines.", "culture", now(), now()),
+("I believe in the importance of art.", "music, culture", now(), now()),
+("I go out of my way to attend music events.", "music, culture", now(), now()),
+("I am full of ideas about novels.", "culture", now(), now()),
+("I love writing poetry.", "music, culture", now(), now()),
+("I love musical theater.", "music, culture", now(), now()),
+("I believe in aliens.", "science, technology", now(), now()),
+("I love Dance.", "music, culture", now(), now()),
+("I have a large collection of movies.", "music, culture", now(), now()),
+("I am good at making impromptu speeches.", "science, culture", now(), now()),
+("I am a spiritual person.", "culture", now(), now()),
+("Learning body language is very important.", "culture", now(), now()),
+("I don't like to dress up in costumes.", "engineering", now(), now()),
+("I listen to music more than a hour a day", "engineering", now(), now()),
+("I show an attention to detail", "science, technology, engineering", now(), now());
 
-drop table Rawscores
+
 
 insert into Rawscores (user_id, score, category, question_id, created_at, updated_at) values 
-(1,4, "CAT A", 1, now() , now() ),
-(1, 30, "CAT A", 2, now() , now() ),
-(1, 4, "CAT B", 3, now() , now() ),
-(1,1, "CAT B",4, now() , now() ),
-(1, 3, "CAT C", 5, now() , now() ),
-(2,4, "CAT A", 1, now() , now() ),
-(2, 3, "CAT A", 2, now() , now() ),
-(2, 3,"CAT B", 3, now() , now() ),
-(2,5, "CAT C",4, now() , now() ),
-(2, 3, "CAT C",5,now() , now() );
+(1,4, "SCIENCE", 1, now() , now() ),
+(1, 30, "MUSIC", 2, now() , now() ),
+(1, 4, "TECHNOLOGY", 3, now() , now() ),
+(1,1, "CULTURE",4, now() , now() ),
+(1, 3, "ENGINEERING", 5, now() , now() );
 
 
-insert into Aggregatescores (user_id, score, category, created_at, updated_at) values 
-(1,10, "CAT A", now() , now() ),
-(1, 3,"CAT B", now() , now() ),
-(1, 4, "CAT C", now() , now() ),
-(1,1, "CAT D", now() , now() ),
-(1, 3, "CAT E",  now() , now() ),
-(2,10, "CAT A",  now() , now() ),
-(2, 3,"CAT B", now() , now() ),
-(2, 4, "CAT C",  now() , now() ),
-(2,1, "CAT D", now() , now() ),
-(2, 3, "CAT E", now() , now() );
+
 
 
 
@@ -65,7 +66,6 @@ insert into Nerdlevels  (nerd_level, max_score, min_score) values
 
 select * from Users;
 select * from Rawscores;
-select * from Aggregatescores;
 select * from Nerdlevels;
 select * from Categories;
 select * from Questions;
