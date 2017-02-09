b68aab1ad4d1bdaf9113c80bf1113293cc1afa23
Jessica Hornsten <hornsten@users.noreply.github.com>
2017-02-09 12:02:44 -0600

    Forgot password modal, more styling

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,7 +1,26 @@
 setTimeout(function() {
 
     $('#myModal').modal('show');
-}, 500);
+}, 1000 * 10);
+
+function showPassword() {
+
+    var key_attr = $('#key').attr('type');
+
+    if (key_attr != 'text') {
+
+        $('.checkbox').addClass('show');
+        $('#key').attr('type', 'text');
+
+    } else {
+
+        $('.checkbox').removeClass('show');
+        $('#key').attr('type', 'password');
+
+    }
+
+}
+
 
 
 // .modal-backdrop classes
