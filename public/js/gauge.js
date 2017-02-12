9e1e5d4de5411a5945d055baa34c3d2ae6f47a51
Jessica Hornsten <hornsten@users.noreply.github.com>
2017-02-12 13:10:25 -0600

    Set up skeleton for nerd category reveal

--- public/js/gauge.js
+++ public/js/gauge.js
@@ -1,21 +1,21 @@
-
 $.ajaxSetup({
     beforeSend: function(xhr) {
-      xhr.setRequestHeader("Accept", "application/vvv.website+json;version=1");
-      xhr.setRequestHeader("Authorization", "Bearer " + getCookie('auth_token'));
+        xhr.setRequestHeader("Accept", "application/vvv.website+json;version=1");
+        xhr.setRequestHeader("Authorization", "Bearer " + getCookie('auth_token'));
     }
 });
 
 console.log("cookie", getCookie('auth_token'));
+
 function getCookie(c_name) {
     if (document.cookie.length > 0) {
         c_start = document.cookie.indexOf(c_name + "=");
         if (c_start != -1) {
-                c_start = c_start + c_name.length + 1;
-                c_end = document.cookie.indexOf(";", c_start);
-                    if (c_end == -1) {
-                    c_end = document.cookie.length;
-                }
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
             return unescape(document.cookie.substring(c_start, c_end));
         }
     }
@@ -27,7 +27,45 @@ function getCookie(c_name) {
 $.get('/category/nerd', function(data) {
     console.log("cookie", getCookie('auth_token'));
 
-    console.log("data",data);
+    console.log("data", data);
+
+});
+
+$.get('/cat', function(data) {
+
+    var nerdType = data['0'].category;
+    var nerdDiv = $('#nerd-category');
+
+    var nerdCategories = [{
+
+        category: "CULTURE",
+        answer: '<p>You are a Culture Nerd! Friends sometimes tire of the way you burst into song at random moments, but there is no denying you bring creativity and zest for life to every gathering. While you may occasionally retire to your room to ruminate on the poetry of Rilke, your gregarious nature and love of beauty keep you from becoming too solitary.</p>',
+        img: '<img class="img-circle img-responsive" src="./img/band-nerd.png">'
+    }, {
+        category: 'ENGINEERING',
+        answer: 'You love ENGINEERING',
+        imgsrc: 'assets/images/farley.gif'
+    }, {
+        category: 'MUSIC',
+        answer: 'You love MUSIC',
+        imgsrc: 'assets/images/farley.gif'
+    }, {
+        category: 'SCIENCE',
+        answer: 'You love SCIENCE',
+        imgsrc: 'assets/images/farley.gif'
+    }, {
+        category: 'TECHNOLOGY',
+        answer: 'You love technology',
+        imgsrc: 'assets/images/farley.gif'
+    }];
+
+    $('#nerd-title').html('You are a ' + nerdType + ' Nerd!');
+    if (nerdType = nerdCategories[0].category) {
+
+        nerdDiv.html(nerdCategories[0].answer);
+        // $('#nerd-image').html(nerdCategories[0].img);
+    }
+
 
 });
 
